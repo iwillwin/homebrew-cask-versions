e10d7d0cb6527e15a53afc09dab648a9ebb281cb
iwillwin <gmomotdly@gmail.com>
2022-04-07 15:16:29 +0800

    surge3

--- Casks/surge3.rb
+++ Casks/surge3.rb
@@ -2,6 +2,7 @@ cask "surge3" do
   version "3.5.8-1130"
   sha256 "4b1c620834eb8f664c7624fbb48bd401aa8dbcfa6bf963bc4951972226817367"
 
+  # https://dl.nssurge.com/mac/v3/Surge-latest.zip
   url "https://www.nssurge.com/mac/v#{version.major}/Surge-#{version}.zip"
   appcast "https://www.nssurge.com/mac/v#{version.major}/appcast-signed.xml"
   name "Surge"
