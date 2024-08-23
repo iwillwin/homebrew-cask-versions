a642b39296edd361f59f9b3c0700cabd23771658
iwillwin <gmomotdly@gmail.com>
2024-08-23 14:28:50 +0800

    paste 4.1.3==>4.1.2

--- Casks/paste.rb
+++ Casks/paste.rb
@@ -1,8 +1,8 @@
 cask "paste" do
-    version "4.1.3"
-    sha256 "f9a52fa35b8383161d6cd3d4849e1d830d496a08fde6de40e692036ba8437f65"
+    version "4.1.2"
+    sha256 "0e673d24307325e4cf997b03a390cd4812d479ee2cf1011c81c35f7896b55515"
 
-    url "file://#{ENV["HOME"]}/aDrive/app/Paste/Paste_4.1.3.dmg"
+    url "file://#{ENV["HOME"]}/aDrive/app/Paste/Paste_4.1.2.dmg"
     name "Paste"
     desc "The clipboard as it should be. Paste is a new way to keep, search, and organize everything you copy on your Mac, iPhone, and iPad."
     homepage "https://pasteapp.io/"
