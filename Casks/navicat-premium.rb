216d1b0ed43d5b40812ce53e7f6fbaa7f5d8fe6d
iwillwin <gmomotdly@gmail.com>
2025-11-17 14:43:04 +0800

    fix: navicat-premium

--- Casks/navicat-premium.rb
+++ Casks/navicat-premium.rb
@@ -1,8 +1,8 @@
 cask 'navicat-premium' do
-  version '17.1.9'
+  version "17.3.5"
   sha256 '016d16fca941c28e0078ab237ce0f1d5ce8922e814ae0ffc93b9e46d5ae40afa'
 
-  url "file:///Volumes/qing/aDrive/app/Navicat%20Premium/navicat#{version.major_minor.no_dots}_premium_en.dmg"
+  url "file:///Volumes/qing/aDrive/app/Navicat%20Premium/navicat#{version.major_minor}_premium_en.dmg"
   name "Navicat Premium"
   desc "Database administration and development tool"
   homepage "https://www.navicat.com/products/navicat-premium"
