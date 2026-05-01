b1a2aa5e88faed36d15556286c89065f08245a01
iwillwin <gmomotdly@gmail.com>
2026-05-02 07:19:28 +0800

    sogou-input 620a

--- Casks/sogou-input.rb
+++ Casks/sogou-input.rb
@@ -1,11 +1,48 @@
-cask 'sogouinput' do
-  version '6.20.0'
-  sha256 'dbb7ac37b7a54723a538fe316b28155c11e1177db637b21a8445b2b6fd7d6d31'
+cask "sogou-input" do
+  version "620a"
 
-  url "https://ime-sec.gtimg.com/202605020712/4ffd335260cb4f743be091ce7ad15bf2/pc/sogou_mac_new_guanwang_620a.zip"
+  # sha256
+
+  url "https://rabbit-linker.vercel.app/gtimg/sogou_mac/#{version}",
+      verified: "rabbit-linker.vercel.app/"
+  name "Sogou Input Method"
   name "搜狗输入法"
-  desc "搜狗输入法是搜狗公司推出的一款输入法软件，支持多种输入方式和丰富的词库，提供流畅的输入体验。"
+  desc "Input method supporting full and double spelling"
   homepage "https://pinyin.sogou.com/mac/"
 
-  app "搜狗输入法.app"
+  livecheck do
+    url :homepage
+    # https://ime-sec.gtimg.com/{dt}/{foo}/pc/dl/gzindex/{ts}/sogou_mac_617a.zip
+    regex(%r{href="https://ime-sec.gtimg.com/[^"]+/sogou_mac_(\d+(?:\.\d+)*[a-z]*)\.zip}i)
+    strategy :page_match do |page|
+      page.scan(regex).map { |match| match&.first }
+    end
+  end
+
+  auto_updates true
+
+  installer manual: "sogou_mac_#{version}.app"
+
+  uninstall launchctl: "com.sogou.SogouServices",
+            delete:    [
+              "/Library/Input Methods/SogouInput.app",
+              "/Library/QuickLook/SogouSkinFileQuickLook.qlgenerator",
+            ]
+
+  zap trash: [
+        "~/.sogouinput",
+        "~/Library/Application Support/Sogou/EmojiPanel",
+        "~/Library/Application Support/Sogou/InputMethod",
+        "~/Library/Caches/com.sogou.inputmethod.sogou",
+        "~/Library/Caches/com.sogou.SGAssistPanel",
+        "~/Library/Caches/com.sogou.SogouPreference",
+        "~/Library/Caches/SogouServices",
+        "~/Library/Cookies/com.sogou.inputmethod.sogou.binarycookies",
+        "~/Library/Cookies/com.sogou.SogouPreference.binarycookies",
+        "~/Library/Cookies/SogouServices.binarycookies",
+        "~/Library/Preferences/com.sogou.SogouPreference.plist",
+        "~/Library/Saved Application State/com.sogou.SogouInstaller.savedState",
+      ],
+      rmdir: "~/Library/Application Support/Sogou"
 end
+# https://github.com/Homebrew/homebrew-cask/pull/155138
