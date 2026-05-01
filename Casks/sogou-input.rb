10a2504e2fa67b4e33bd66c35dc6c40b6e816e3f
iwillwin <gmomotdly@gmail.com>
2026-05-02 07:17:59 +0800

    sogou-input

--- Casks/sogou-input.rb
+++ Casks/sogou-input.rb
@@ -2,7 +2,7 @@ cask 'sogouinput' do
   version '6.20.0'
   sha256 'dbb7ac37b7a54723a538fe316b28155c11e1177db637b21a8445b2b6fd7d6d31'
 
-  url "https://ime-sec.gtimg.com/202605020659/99edf52c923a84de5fa928c2fc46617b/pc/sogou_mac_new_guanwang_620a.zip"
+  url "https://ime-sec.gtimg.com/202605020712/4ffd335260cb4f743be091ce7ad15bf2/pc/sogou_mac_new_guanwang_620a.zip"
   name "搜狗输入法"
   desc "搜狗输入法是搜狗公司推出的一款输入法软件，支持多种输入方式和丰富的词库，提供流畅的输入体验。"
   homepage "https://pinyin.sogou.com/mac/"
