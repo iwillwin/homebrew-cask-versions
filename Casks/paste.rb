820c6dc979efbc9e3a811c58b19746fa506e5d26
iwillwin <gmomotdly@gmail.com>
2026-05-01 18:48:57 +0800

    fix: paste 4.4.2 ==> 6.4.0

--- Casks/paste.rb
+++ Casks/paste.rb
@@ -1,13 +1,11 @@
 cask "paste" do
-  version "4.4.2"
+  version "6.4.0"
   sha256 "487d3a52388300a93522ad89aa89f7af73375eba7cf924ee9d029e53f8069c60"
 
-  url "file:///Volumes/qing/aDrive/app/Paste/Paste_4.4.2.dmg"
+  url "file:///Volumes/qing/aDrive/app/Paste/Paste_6.4.0.dmg"
   name "Paste"
   desc "The clipboard as it should be. Paste is a new way to keep, search, and organize everything you copy on your Mac, iPhone, and iPad."
   homepage "https://pasteapp.io/"
-
-  depends_on macos: ">= :mojave"
 
   app "Paste.app"
 end
